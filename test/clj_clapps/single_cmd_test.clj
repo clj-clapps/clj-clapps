8d19b072c172b9e415a425b43f99e13b05307d79
Julio Rincon <jcrinconc@gmail.com>
2021-05-05 22:01:49 +1000

    Fix readme typos and more tests

---
+++ test/clj_clapps/single_cmd_test.clj
@@ -0,0 +1,29 @@
+(ns clj-clapps.single-cmd-test
+  (:require [clj-clapps.core :as cl :refer [defcmd defopt]]
+            [clojure.string :as str]
+            [clojure.test :refer :all])
+  (:import java.time.LocalDate))
+
+(defopt user "the service username" :short "-u" :env "USER")
+(defopt pass "the service password" :short "-p" :env "PASS")
+
+(defcmd main-cmd
+  "the only command"
+  [^{:parse-fn read-string :validate [int? "must be a number"]} arg1
+   & [^{:doc "Date option" :short "-d" :default-fn (constantly (LocalDate/now))} date]]
+  (pr arg1 date))
+
+(def this-ns *ns*)
+
+(deftest test-single-cmd
+  (testing "usage output"
+    (let [parse-main-args #'clj-clapps.core/parse-main-args
+          parse-cmd-args #'clj-clapps.core/parse-cmd-args
+          cmd #'main-cmd]
+      (is (like? {:command {:name 'main-cmd}
+                  :options {:help nil?}
+                  :main? true}
+                 (parse-main-args this-ns [])))
+      (is (like? {:exit-message #(str/includes? % "must be a number")}
+                 (parse-cmd-args (meta cmd) ["abc"] true ""))))))
+
