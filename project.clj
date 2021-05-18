609dc8eb7e99559e34060b76a57bf362f727af15
Julio Rincon <jcrinconc@gmail.com>
2021-05-19 09:14:00 +1000

    Version 0.4.11

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.clj-clapps/clj-clapps "0.4.10-SNAPSHOT"
+(defproject org.clojars.clj-clapps/clj-clapps "0.4.11"
   :description "A library to create command line apps with ease and elegance"
   :url "https://github.com/clj-clapps/clj-clapps"
   :license {:name "Eclipse Public License"
